5e53d353ca0d7c42329f80db9b60fade5890abc4
PKNate <arturoraal@hotmail.es>
2022-05-15 22:02:28 -0500

    running out of names for these commits

--- elgato.c
+++ elgato.c
@@ -30,6 +30,14 @@
 
 int16 L, FL, F, FR, R; 
 //int32 temp, x, y, z;
+float pwmL, pwmR;
+float k=0.3;
+int16 minV=150;
+int16 maxV=300;
+int16 limWall=260;
+int16 noWall=100;
+
+
 volatile int8 aux;
 volatile int8 encoderM1=0;
 volatile int8 encoderM1_anterior=0;
@@ -67,7 +75,7 @@ void rb_isr(){
 
 void setup(short on);
 void fflush();
-void motor(char M1, char M2, int pwm1, int pwm2);
+void motor(int M1, signed int16 pwm1, char M2, signed int16 pwm2);
 void readSensor(int sensor);
 void debug(int type);
 
@@ -79,6 +87,179 @@ void main()
    while(true)
    {
       debug(1);
+      //readSensor(5);
+      /*
+      if(F<300)
+      {
+         STBY=1;
+         pwmL=200;
+         pwmR=200;
+         
+         if(L>noWall && R>noWall)
+         {
+            if(L>R)
+            {
+               pwmL=pwmL+(k*(L-R));
+               pwmR=pwmR-(k*(L-R));
+            }
+            
+            else if(R>L)
+            {
+               pwmL=pwmL-(k*(R-L));
+               pwmR=pwmR+(k*(R-L));
+            }
+         }
+         
+         else if(L<noWall) //no left wall
+         {
+            if(R>limWall)
+            {
+               pwmL=pwmL-(k*(R-limWall));
+               pwmR=pwmR+(k*(R-limWall));
+            }
+            
+            if(R<limWall)
+            {
+               pwmL=pwmL+(k*(R-limWall));
+               pwmR=pwmR-(k*(R-limWall));
+            }
+         }
+         
+         else if(R<noWall) //no left wall
+         {
+            if(L>limWall)
+            {
+               pwmL=pwmL-(k*(L-limWall));
+               pwmR=pwmR+(k*(L-limWall));
+            }
+            
+            if(L<limWall)
+            {
+               pwmL=pwmL+(k*(L-limWall));
+               pwmR=pwmR-(k*(L-limWall));
+            }
+         }
+         
+
+         
+         motor('D',(signed int16)pwmL,'D',(signed int16)pwmR);
+      }
+      
+      else
+      STBY=0;
+      */
+   }
+}
+
+void setup(short on)
+{
+   STBY = 0;
+   enable_interrupts(int_rb);
+   enable_interrupts(int_rda);
+   enable_interrupts(global);
+   
+   setup_adc(adc_clock_div_16);
+   setup_adc_ports(AN0_TO_AN4, VSS_VDD);
+   set_adc_channel(0);
+   delay_us(20);
+   
+   //mpu6050_init();
+   
+   TRISA=0b11111111;
+   TRISB=0b11111101; //SCL
+   TRISC=0b10111001; //PWMA, PWMB, TX
+   TRISD=0b11111100; //BI1, BI2
+   TRISE=0b01111000; //STBY, AI1, AI2
+   
+   setup_timer_2(T2_DIV_BY_1,255,1);
+   setup_ccp1(CCP_PWM);
+   setup_ccp2(CCP_PWM);
+   
+   if(on)
+   {
+      motor('D',(int16)200,'D',(int16)200);
+      STBY = 1;
+   }
+   
+   return;
+}
+
+void fflush()
+{
+   if(OERR)
+   {
+      getc();     //Clear buffer
+      getc();
+      CREN=0;      //Clear CREN bit
+      CREN=1;           
+   }
+}
+
+void motor(int M1, signed int16 pwm1, char M2, signed int16 pwm2)
+{
+   switch (M1)
+   {
+      case 'D': {BI1=1; BI2=0; break;}
+      case 'R': {BI1=0; BI2=1; break;}
+      case 'N': {BI1=0; BI2=0; break;}
+   }
+   
+   switch (M2)
+   {
+      case 'D': {AI1=1; AI2=0; break;}
+      case 'R': {AI1=0; AI2=1; break;}
+      case 'N': {AI1=0; AI2=0; break;}
+   }
+   
+   if(pwm1>maxV) pwm1=maxV;
+   else if(pwm1<minV) pwm1=minV;
+   if(pwm2>maxV) pwm2=maxV;
+   else if(pwm2<minV) pwm2=minV;
+   
+   set_pwm1_duty(pwm1);
+   set_pwm2_duty(pwm2);
+}
+
+void readSensor(int sensor)
+{
+   switch (sensor)
+   {
+      case 0: {set_adc_channel(1); delay_us(20); L=read_adc(); break;}
+      case 1: {set_adc_channel(4); delay_us(20); FL=read_adc(); break;}
+      case 2: {set_adc_channel(3); delay_us(20); F=read_adc(); break;}
+      case 3: {set_adc_channel(2); delay_us(20); FR=read_adc(); break;}
+      case 4: {set_adc_channel(0); delay_us(20); R=read_adc(); break;}
+      case 5: {set_adc_channel(1); delay_us(20); L=read_adc();
+               set_adc_channel(4); delay_us(20); FL=read_adc();
+               set_adc_channel(3); delay_us(20); F=read_adc();
+               set_adc_channel(2); delay_us(20); FR=read_adc();
+               set_adc_channel(0); delay_us(20); R=read_adc(); break;}
+   } 
+}
+
+void debug(int type)
+{
+   switch(type)
+   {
+      case 0:
+      {
+        readSensor(5);   
+        printf("M1: %Ld    M2: %Ld \r\n",cuentasM1, cuentasM2);
+        delay_ms(100);
+        break;
+      }
+      
+      case 1:
+      {
+        readSensor(5);   
+        printf("L:%Ld\tFL:%Ld\tF:%Ld\tFR:%Ld\tR:%Ld\n\r",L,FL,F,FR,R);
+        delay_ms(100);
+        break;
+      }
+   }
+   return;
+}
+
       /*
       readSensor(5);
       if(F>210)
@@ -128,109 +309,3 @@ void main()
          motor('D',50,'D',50);
       }
       */
-   }
-}
-
-void setup(short on)
-{
-   STBY = 0;
-   enable_interrupts(int_rb);
-   enable_interrupts(int_rda);
-   enable_interrupts(global);
-   
-   setup_adc(adc_clock_div_16);
-   setup_adc_ports(AN0_TO_AN4, VSS_VDD);
-   set_adc_channel(0);
-   delay_us(20);
-   
-   //mpu6050_init();
-   
-   TRISA=0b11111111;
-   TRISB=0b11111101; //SCL
-   TRISC=0b10111001; //PWMA, PWMB, TX
-   TRISD=0b11111100; //BI1, BI2
-   TRISE=0b01111000; //STBY, AI1, AI2
-   
-   setup_timer_2(T2_DIV_BY_1,255,1);
-   setup_ccp1(CCP_PWM);
-   setup_ccp2(CCP_PWM);
-   
-   if(on)
-   {
-      motor('D',50,'D',50);
-      STBY = 1;
-   }
-   
-   return;
-}
-
-void fflush()
-{
-   if(OERR)
-   {
-      getc();     //Clear buffer
-      getc();
-      CREN=0;      //Clear CREN bit
-      CREN=1;           
-   }
-}
-
-void motor(int M1, int pwm1, char M2, int pwm2)
-{
-   switch (M1)
-   {
-      case 'D': {BI1=1; BI2=0; break;}
-      case 'R': {BI1=0; BI2=1; break;}
-      case 'N': {BI1=0; BI2=0; break;}
-   }
-   
-   switch (M2)
-   {
-      case 'D': {AI1=1; AI2=0; break;}
-      case 'R': {AI1=0; AI2=1; break;}
-      case 'N': {AI1=0; AI2=0; break;}
-   }
-   
-   set_pwm1_duty(pwm1);
-   set_pwm2_duty(pwm2);
-}
-
-void readSensor(int sensor)
-{
-   switch (sensor)
-   {
-      case 0: {set_adc_channel(1); delay_us(20); L=read_adc(); break;}
-      case 1: {set_adc_channel(4); delay_us(20); FL=read_adc(); break;}
-      case 2: {set_adc_channel(3); delay_us(20); F=read_adc(); break;}
-      case 3: {set_adc_channel(2); delay_us(20); FR=read_adc(); break;}
-      case 4: {set_adc_channel(0); delay_us(20); R=read_adc(); break;}
-      case 5: {set_adc_channel(1); delay_us(20); L=read_adc();
-               set_adc_channel(4); delay_us(20); FL=read_adc();
-               set_adc_channel(3); delay_us(20); F=read_adc();
-               set_adc_channel(2); delay_us(20); FR=read_adc();
-               set_adc_channel(0); delay_us(20); R=read_adc(); break;}
-   } 
-}
-
-void debug(int type)
-{
-   switch(type)
-   {
-      case 0:
-      {
-        readSensor(5);   
-        printf("M1: %Ld    M2: %Ld \r\n",cuentasM1, cuentasM2);
-        delay_ms(100);
-        break;
-      }
-      
-      case 1:
-      {
-        readSensor(5);   
-        printf("L:%Ld\tFL:%Ld\tF:%Ld\tFR:%Ld\tR:%Ld\n\r",L,FL,F,FR,R);
-        delay_ms(100);
-        break;
-      }
-   }
-   return;
-}
